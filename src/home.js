f5da05fcde771b3d778f6cc00e417a47e3dd6738
Erik <erik.p.schmitt@gmail.com>
2022-08-14 21:41:07 +0200

    Added home page.

---
+++ src/home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <a href="/ranking">ranking</a>
+      <div></div>
+      <a href="/voting/1">voting</a>
+    </div>
+  );
+};
+
+export default Home;
